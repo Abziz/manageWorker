07fab2d5507406c7dc71e64e9f942b0a66da0bdc
Omry Ben David <omryb@mellanox.com>
2020-08-31 02:16:08 +0300

    Create and switch to branch Deploy
    Deployment is fully loaded.
    need to modify and create more html files.

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,9 +1,89 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const { response } = require('express');
+const app = express();
+let fs = require('fs');
+// build multiple CRUD interfaces:
+app.use(express.static(__dirname));
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+app.get('/', (req, res) => {
+  fs.readFile(__dirname + '/index.html', null, (error, data) => {
+    if (error) {
+      res.writeHead(404);
+      // eslint-disable-next-line no-undef
+      res.write('Whoops! File not found!');
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
 });
+
+app.get('/worker', (req, res) => {
+  fs.readFile(__dirname + '/workerPage.html', null, (error, data) => {
+    if (error) {
+      res.writeHead(404);
+      // eslint-disable-next-line no-undef
+      res.write('Whoops! File not found!');
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
+});
+
+app.get('/forgotPass', (req, res) => {
+  fs.readFile(__dirname + '/forgotPass.html', null, (error, data) => {
+    if (error) {
+      res.writeHead(404);
+      // eslint-disable-next-line no-undef
+      res.write('Whoops! File not found!');
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
+});
+
+app.get('/login', (req, res) => {
+  fs.readFile(__dirname + '/index.html', null, (error, data) => {
+    if (error) {
+      res.writeHead(404);
+      // eslint-disable-next-line no-undef
+      res.write('Whoops! File not found!');
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
+});
+
+app.get('/signup', (req, res) => {
+  fs.readFile(__dirname + '/signup.html', null, (error, data) => {
+    if (error) {
+      res.writeHead(404);
+      // eslint-disable-next-line no-undef
+      res.write('Whoops! File not found!');
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
+});
+
+app.get('/timestamp', (request, response) => {
+  response.send(`${Date.now()}`);
+});
+     
+exports.app = functions.https.onRequest(app);
+
+// const express = require("express");
+// const admin = require("firebase-admin");
+
+// var serviceAccount = require("path/to/serviceAccountKey.json");
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://sacnnershop.firebaseio.com"
+// });
