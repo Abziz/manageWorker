220095d20f98c8c9edfc7e1a1b2ee934368242c0
omry777 <63002593+omry777@users.noreply.github.com>
2020-09-05 14:51:31 +0300

    Deploy (#4)
    * *Login page Front_End
    *Worker page basic
    
    Signed-off-by: Yair Charit <yaircharit@gmail.com>
    
    * A rough front-end of the project
    + Login Page
    + Sign-up Page
    + Worker Page
    
    *still need to add real functioanallity (check fields, interact with DB,
    etc..)
    
    Signed-off-by: Yair Charit <yaircharit@gmail.com>
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * add packagesJsons
    
    * Delete server.js
    
    * Delete index.html
    
    * Update package files for eslint correct work
    
    * Firebase Integration.
    
    Deployed first skeleton for index at
    https://sacnnershop.web.app/
    
    Signed-off-by: Omry Ben David <omryb@mellanox.com>
    
    * Create and switch to branch Deploy
    
    Deployment is fully loaded.
    need to modify and create more html files.
    
    * added menu, seperated common HTML files to access from  multiple pages
    
    Signed-off-by: Yair Charit <yaircharit@gmail.com>
    
    Co-authored-by: Yair Charit <yaircharit@gmail.com>
    Co-authored-by: AvivWeiz <44341264+AvivWeiz@users.noreply.github.com>
    Co-authored-by: Aviv Weizman <AvivW@migal.org.il>
    Co-authored-by: alexk81094 <alexk81094@gmail.com>
    Co-authored-by: Omry Ben David <omryb@mellanox.com>
    Co-authored-by: alexk81094 <57593655+alexk81094@users.noreply.github.com>

---
+++ firebase/functions/index.js
@@ -0,0 +1,89 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const { response } = require('express');
+const app = express();
+let fs = require('fs');
+// build multiple CRUD interfaces:
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  fs.readFile(__dirname + 'html/index.html', null, (error, data) => {
+    if (error) {
+      res.writeHead(404);
+      // eslint-disable-next-line no-undef
+      res.write('Whoops! File not found!');
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
+});
+
+app.get('/worker', (req, res) => {
+  fs.readFile(__dirname + 'html/workerPage.html', null, (error, data) => {
+    if (error) {
+      res.writeHead(404);
+      // eslint-disable-next-line no-undef
+      res.write('Whoops! File not found!');
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
+});
+
+app.get('/forgotPass', (req, res) => {
+  fs.readFile(__dirname + 'html/forgotPass.html', null, (error, data) => {
+    if (error) {
+      res.writeHead(404);
+      // eslint-disable-next-line no-undef
+      res.write('Whoops! File not found!');
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
+});
+
+app.get('/login', (req, res) => {
+  fs.readFile(__dirname + 'html/index.html', null, (error, data) => {
+    if (error) {
+      res.writeHead(404);
+      // eslint-disable-next-line no-undef
+      res.write('Whoops! File not found!');
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
+});
+
+app.get('/signup', (req, res) => {
+  fs.readFile(__dirname + 'html/signup.html', null, (error, data) => {
+    if (error) {
+      res.writeHead(404);
+      // eslint-disable-next-line no-undef
+      res.write('Whoops! File not found!');
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
+});
+
+app.get('/timestamp', (request, response) => {
+  response.send(`${Date.now()}`);
+});
+     
+exports.app = functions.https.onRequest(app);
+
+// const express = require("express");
+// const admin = require("firebase-admin");
+
+// var serviceAccount = require("path/to/serviceAccountKey.json");
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://sacnnershop.firebaseio.com"
+// });
